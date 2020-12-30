a07d66003da1dc27648b9c1e7ab400e24c11e0ee
Domenico <dspoto@hotmail.it>
2020-12-30 11:44:52 +0100

    Updates of preprocessing and py file

---
+++ read_write_pickle.py
@@ -0,0 +1,13 @@
+def write_pickle(file_name, content):
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
+    with open(file_name, 'wb') as handle:
+        pickle.dump(content, handle)
+
+def read_pickle(file_name):
+    with open(file_name, 'rb') as handle:
+        return pickle.load(handle)
+
+
+write_pickle('data/categories_red.pkl', categories_red)
+
+categories_red  = read_pickle('data/categories_red.pkl')
