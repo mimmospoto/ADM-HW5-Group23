eef25cba378a87c1e0e2f011961ddd124b9a9274
Domenico <dspoto@hotmail.it>
2021-01-14 16:32:56 +0100

    Updates final

--- main/functions.py
+++ main/functions.py
@@ -5,6 +5,9 @@ from tqdm import tqdm
 import pickle
 import numpy as np
 from collections import defaultdict, Counter
+from prettytable import PrettyTable
+import itertools
+import scipy.sparse as sp
 
 
 """
@@ -48,8 +51,8 @@ def get_graph_dictionary(data):
             graph[row]
     return(graph)
 
-
 class Graph(object):
+
     def __init__(self, graph_d=None):
         if graph_d == None:
             graph_d = {}
@@ -67,7 +70,6 @@ class Graph(object):
             except TypeError:
                 edges_lst.append((node, self.graph_d[node]))
         return edges_lst
-
 
 def average_number_pages1(g):
     idx = (random.choice(g.vertices()))
@@ -94,13 +96,40 @@ def density_graph(g):
     E = len(g.edges())
     return E / (V *(V - 1))
 
+def IsSymmetric(mat, g):
+    """
+    Build a lil matrix to create a sparse matrix of the vertices and edges,
+    get the sum of the point in the matrix,
+    check if the matrix is symmetric or not
+    """
+    # looping on each vertex to assign the edges == 1
+    for vertex in g.graph_d:   
+        if isinstance(g.graph_d[vertex], int):
+            mat[g.graph_d[vertex], target] = 1
+        else:
+            for target in g.graph_d[vertex]:
+                mat[vertex, target] = 1
+    
+    rows, cols = mat.nonzero() # get only the non zero elements from the sparse matrix 
+    return rows, cols
 
 """
 -------------------------------------------------------------------
 REQUEST 2
 -------------------------------------------------------------------
 """
-
+def pages(page, click, dic):
+    total_pages = [] # This list will store number of pages
+    page_list = [] #This list will store input vlaue initially and then will add correspondence value as per number of click
+    page_list.append(str(page))
+    for no_of_click in range(click): #This will run as per number of clicks
+        new_lst = []                 
+        for i in page_list:
+            for j in dic[i]:
+                new_lst.append(str(j))
+                total_pages.append(str(j))
+        page_list = new_lst
+    return total_pages
 
 """
 -------------------------------------------------------------------
@@ -138,77 +167,108 @@ def most_central_article(category, in_degree_dict):
             continue
     return max_in_degree_vertex, max_in_degree_value, not_degree_article
 
+def minimum_number_clicks(graph, categories_red, data):
+    print('Write the category')
+    while True:
+        category_input = str(input())
+        if category_input not in categories_red:
+            print(category_input, ' not exist as category, change category')
+        else:
+            break
+    print()
+    print("Write the set of pages in the category chosen separated by a ',':")
+    pages_input = input()
+    pages_input = pages_input.split(',')
+    pages_input = [int(i) for i in pages_input]
+
+    print()
+    pages_not = []
+    for pages in pages_input:
+        if pages not in categories_red[category_input]:
+            print(pages, ' not in ', category_input)
+            pages_not.append(pages)
+    pages_input = [i for i in pages_input if i not in pages_not]  
+    
+    graph = graph                       # the graph
+    central_vertex = most_central_article(categories_red[category_input], in_degree_centrality(data))[0]   # set the max vertex
+    v = central_vertex
+
+    visited = [False] * (max(graph) + 1) # set as False the visited vertex
+    queue = []                           # set the queue list
+    queue.append(v)                      # append the starting vertex to the list
+    
+    visited[v] = True                    # set the starting vertex as visited
+    reached = 0                          # initialize the number of reached vertex
+    reached_vertex = []                  # initialize the list of reached vertex
+    number_of_click = 0
+
+    while queue:
+        if reached < (len(pages_input)):
+            v = queue.pop(0)
+            
+            try:
+                number_of_click += 1
+                for i in graph[v]:
+                    if visited[i] == False:
+                        visited[i] = True
+                        queue.append(i)
+                    
+                        if i in pages_input:
+                            reached += 1
+                            reached_vertex.append(i)
+                            
+            except TypeError:
+                number_of_click += 1
+                j = graph[v]
+                if visited[j] == False:
+                    visited[j] = True
+                    queue.append(j)
+                    
+                    if j in pages_input:
+                        reached += 1
+                        reached_vertex.append(j)
+                        
+
+        else:
+            break
+    print('Reached vertex are: {}'.format(reached_vertex))
+    print('Minimum number of clicks, from most central article {} to reach the set of pages, is {}.'.format(central_vertex, number_of_click))
+    not_reached_vertex = [i for i in pages_input if i not in reached_vertex]
+    print('Not possible to reach {}'.format(not_reached_vertex))
 
 """
 -------------------------------------------------------------------
 REQUEST 4
 -------------------------------------------------------------------
 """
-#first function
-def get_graph_dictionary(data):
 
-    concat = [data['Source'], data['Target']]
-    df_concat = pd.concat(concat)
-    data_2 = data.set_index('Source')
+def cat_subgraph(category_1, category_2, categories_dict, dataframe):
 
-
-    graph = defaultdict(list)
-    for row in df_concat.unique():
-        try:
-            graph[row] = data_2.loc[row, 'Target'].tolist()
-        except AttributeError:
-            graph[row] = data_2.loc[row, 'Target'].flatten().tolist()
-        except KeyError:
-            graph[row]
-    return(graph)
-
-def cat_subgraph(c1, c2, df):
-    
     #first get the two lists of pages of the categories
-    a = cat[c1]
-    b = cat[c2]
-    
+    a = categories_dict[category_1]
+    b = categories_dict[category_2]
+
     #given those lists, find the sources for both
-    source_a = df[df['Source'].isin(a)]
-    source_b = df[df['Source'].isin(b)]
-    
+    source_a = dataframe[dataframe['Source'].isin(a)]
+    source_b = dataframe[dataframe['Source'].isin(b)]
+
     #now find the edges, that have as targets the other list
     edges_ab = source_a[source_a['Target'].isin(b)]
     edges_ba = source_b[source_b['Target'].isin(a)]
-    
+
     #edges within the categories
     edges_aa = source_a[source_a['Target'].isin(a)]
     edges_bb = source_b[source_b['Target'].isin(b)]
-    
+
     #put them together
     sub_df = pd.concat([edges_ab, edges_ba, edges_aa, edges_bb])
-    
+
     #convert input graph in a dict and give that as output
     sub_graph = get_graph_dictionary(sub_df)
-    for key,val in sub_graph.items():
-        if isinstance(sub_graph[key], int):
-            sub_graph[key] = [sub_graph[key]]
 
     return sub_graph
 
-def show_first_order_neigbors(G, start_node):
-    try: 
-        sub_nodes = [n for n in G[start_node]]
-        edges_sub = [item for item in g.edges() if item[0] == start_node]
-        G_sub = nx.DiGraph()
-        G_sub.add_nodes_from(sub_nodes)
-        G_sub.add_edges_from(edges_sub)
-        pos = nx.spring_layout(G_sub)
-        nx.draw_networkx_nodes(G_sub, pos, cmap=plt.get_cmap('jet'), 
-                               node_color = "orange", node_size = 800)
-        nx.draw_networkx_labels(G_sub, pos)
-        nx.draw_networkx_edges(G_sub, pos, arrows=True)
-        plt.show()
-    except:
-        print("No link avaible")
 
-
-# second function
 def find_hyperlinks(graph, u, v, link=[]):
         link = link + [u]
         final_links = []
@@ -217,9 +277,11 @@ def find_hyperlinks(graph, u, v, link=[]):
         for i in graph[u]:
             if i not in link:
                 other_links = find_hyperlinks(graph, i, v, link)
-                for l in other_links: 
+                for l in other_links:
                     final_links.append(l)
         return final_links
+
+
 
 def min_hyperlinks(graph, u, v):
     return len(find_hyperlinks(graph, u, v))
@@ -230,31 +292,28 @@ REQUEST 5
 -------------------------------------------------------------------
 """
 
-def relevant_pages(c, df):
-    a = cat[c]
-    
-    d1 = df[df['Source'].isin(a)]
-    d2 = df[df['Target'].isin(a)]
-    
+def relevant_pages(category,categories_dict,  dataframe):
+    a = categories_dict[category]
+    d1 = dataframe[dataframe['Source'].isin(a)]
+    d2 = dataframe[dataframe['Target'].isin(a)]
     concat = [d1['Source'], d2['Target']]
     df_concat = pd.concat(concat)
     d = list(df_concat.unique())
-    
+
     return d
 
-def page_distance(start, category, df, sub_graph, visited, distance):
+
+def page_distance(start, visited, distance,sub_graph, category, categories_dict, dataframe):
+
     queue = [] + [start]
-    order = 0
-    visited[start] = True 
-    distance[start] = order
-    l = relevant_pages(category, df)
+    visited[start] = True
+    distance[start] = 0
+    l = relevant_pages(category, categories_dict, dataframe)
     distances = []
-    
+
     while len(queue) > 0 :
-        order += 1     
         z = queue[0]
         queue.pop(0)
-        
         for i in sub_graph[z]:
             if i in l:
                 if visited[i] == False:
@@ -264,7 +323,48 @@ def page_distance(start, category, df, sub_graph, visited, distance):
                     distance [i] = distance[z] + 1
                     distances.append(distance[i])
 
-    return distances
+    return (distances)
+
+def distances_from_category(input_category,categories_dict, dataframe, all_nodes ):
+    results = {}
+
+    for cat1 in tqdm(categories_dict.keys()):
+        if cat1 != input_category:
+            visited = dict.fromkeys(all_nodes, False)
+            distance = dict.fromkeys(all_nodes, float('inf'))
+            sub_graph = cat_subgraph(input_category, cat1, categories_dict, dataframe)
+
+            for key,val in sub_graph.items():
+                if isinstance(sub_graph[key], int):
+                    sub_graph[key] = [sub_graph[key]]
+            pages = relevant_pages(cat1,categories_dict, dataframe)
+            aux = []
+            for i in pages:
+                x = page_distance(i,visited, distance,sub_graph,
+                                  input_category, categories_dict, dataframe)
+                if x:
+                    aux.append(x)
+            merged = np.array(list(itertools.chain(*aux)))
+            m = np.median(merged)
+            print(m)
+            results[cat1] = m
+    write_pickle('data/' + input_category, results)
+    return (results)
+
+
+def score_categories(category_name):
+    if isinstance(category_name, str):
+        cat_name = "data/"+category_name+".pkl"
+        ll = read_pickle(cat_name)
+        d_view = [ (v,k) for k,v in ll.items() ]
+        d_view.sort(reverse=False) # natively sort tuples by first element
+        t = PrettyTable(['Categories', 'Distance'])
+        for v,k in d_view:
+            if not np.isnan(v):
+                t.add_row([k,v])
+        print(t)
+    else:
+        print("Category name must be string")
 
 
 
@@ -278,7 +378,7 @@ def out_degree_centrality(g):
     for key in g.graph_d:
         if isinstance(g.graph_d[key], list):
             out_deg_list.append(len(g.graph_d[key]))
-        elif (isinstance(g.graph_d[key], int)): 
-            out_deg_list.append(1) 
+        elif (isinstance(g.graph_d[key], int)):
+            out_deg_list.append(1)
     out_deg = Counter(sorted(out_deg_list))
     return out_deg
--- read_write_pickle.py
+++
@@ -1,9 +0,0 @@
-def write_pickle(file_name, content):
-    os.makedirs(os.path.dirname(file_name), exist_ok=True)
-    with open(file_name, 'wb') as handle:
-        pickle.dump(content, handle)
-
-def read_pickle(file_name):
-    with open(file_name, 'rb') as handle:
-        return pickle.load(handle)
-
